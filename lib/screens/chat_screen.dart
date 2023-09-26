3385ad2475314e5a26c817b9bc86fe80bc34f693
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-09-26 08:16:38 +0600

    Adding Base of chat screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+      return Scaffold(
+         body : ListView.builder(
+          itemCount: 10,
+          itemBuilder: (ctx,index) => Container(
+            padding: EdgeInsets.all(4),
+            child: Text('this works'),
+          ),
+       ),
+    );
+  }
+}
