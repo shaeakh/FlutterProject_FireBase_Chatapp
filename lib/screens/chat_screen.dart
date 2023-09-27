9b71aafeea631d1c44da50497d1f93745c7b83af
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-09-27 10:49:35 +0600

    .

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget{
   @override
@@ -9,9 +10,17 @@ class ChatScreen extends StatelessWidget{
           itemCount: 10,
           itemBuilder: (ctx,index) => Container(
             padding: EdgeInsets.all(4),
-            child: Text('this works'),
+            child: Text('thiss works'),
           ),
        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: (){
+            FirebaseFirestore.instance
+                .collection('/chats/iieHhDuNLGWgmF0Ea10L/messeges')
+                .snapshots()
+          },
+        )
     );
   }
 }
